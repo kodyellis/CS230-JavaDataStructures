c1c76cbbb1f3c48dd9689a44181dcc05f77c9711
Kody Ellis <kellis5099@tuskegee.edu>
2018-01-12 19:46:07 -0600

    Update lab31.java
    corrected code and documentation.

--- lab3/lab31.java
+++ lab3/lab31.java
@@ -1,6 +1,5 @@
 /**
-  file name -- ShowRead01.java
-  This program shows how to read data from the keyboard in Java.
+  file name -- lab31.java
 */
 
 import java.io.*;
@@ -16,16 +15,16 @@ class lab31
       int a;
 
       System.out.println("The Six Numbers Are: ");
-      a = rand.nextInt(54);
+     
 
       //for loops
       for(int i = 0; i < 6; i++)
       {
          //do the randomization
-//while a == 0
-//a = rand.Int
-//System print out
-               a = rand.nextInt(3-1+1)+1;
+        //need values from 1 to 54. So .nextInt(53) does 0 - 53
+        //the 0 itself is set inside the function, so to bypass this we add +1 after than function
+        //this way if the funtion returned 0 it would be 1, if it returned 53 it would return 54.
+               a = rand.nextInt(53)+1;
                System.out.println(a + " ");
 
       }
